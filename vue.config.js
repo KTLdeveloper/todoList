78ee72e818d99d47ae2dbfe577ea5e7006d751fe
DavisLin <davislin@trevi.cc>
2019-11-09 23:22:28 +0800

    add webpack chunk max&min size

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    },
     plugins: [
       new BundleAnalyzerPlugin({
         analyzerMode: process.env.NODE_ENV === 'production' ? 'static' : 'server',
