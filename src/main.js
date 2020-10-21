4810ef70015325996772c932160e097ddf6f86cf
DavisLin <davislin@trevi.cc>
2020-10-21 18:22:05 +0800

    add hotjar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import hotjar from './plugins/hotjar'
 import router from './router'
 import store from './store'
 
@@ -8,6 +9,7 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  hotjar,
   router,
   store,
   render: h => h(App)
---
+++ src/plugins/hotjar.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Hotjar from 'vue-hotjar'
+
+Vue.use (Hotjar, {
+  id: '2052881', // Hotjar Site ID
+  isProduction: true
+})
+
